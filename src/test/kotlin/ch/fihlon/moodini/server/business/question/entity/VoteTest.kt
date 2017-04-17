77b9c389e5f74666ead2c43ba5ceb08b2bad8daf
Marcus Fihlon <marcus@fihlon.ch>
2017-04-17 17:44:40 +0200

    :white_check_mark: Adding tests for the vote entity

---
+++ src/test/kotlin/ch/fihlon/moodini/server/business/question/entity/VoteTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Moodini
+ * Copyright (C) 2017 Marcus Fihlon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.fihlon.moodini.server.business.question.entity
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldNotBe
+import io.kotlintest.specs.StringSpec
+
+class VoteTest : StringSpec() {
+
+    init {
+        val vote = Vote("21", "42", Answer.GOOD)
+
+        "vote should be instantiated" {
+            vote shouldNotBe null
+        }
+
+        "vote ID should be set" {
+            vote.voteId shouldBe "21"
+        }
+
+        "question ID should be set" {
+            vote.questionId shouldBe "42"
+        }
+
+        "answer should be set" {
+            vote.answer shouldBe Answer.GOOD
+        }
+    }
+
+}
